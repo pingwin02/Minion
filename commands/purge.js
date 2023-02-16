69f9884e88a3fcfb9aa7756d2372ef0bed127e07
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-02-16 03:50:55 +0100

    Some fixes

--- commands/purge.js
+++ commands/purge.js
@@ -7,16 +7,16 @@ module.exports = {
     .addIntegerOption(option =>
 		option.setName('ilość')
 			.setDescription('Ilość wiadomości do wykasowania')
-            .setMaxValue(100)
+            .setMaxValue(99)
             .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
   async execute(interaction) {
-
-    const amount = interaction.options.getInteger('ilość');
-
+    await interaction.deferReply();
+    const amount = interaction.options.getInteger('ilość') + 1;
     await interaction.channel.bulkDelete(amount);
-    await interaction.reply(`Usunięto **${amount}** wiadomości.`);
-    setTimeout(() => interaction.deleteReply(), 3000);
 
+    const msg = `Usunięto **${amount-1}** wiadomości.`;
+    interaction.channel.send(msg).then((msg) => {
+        setTimeout(() => msg.delete(), 3000)});
   }
 }
