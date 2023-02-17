ff6375e563aebc07d328e99deeac9e2fc6ada0d2
Damian Jankowski <108282259+pingwin02@users.noreply.github.com>
2023-02-17 18:53:58 +0000

    hidden ping output

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName("ping")
     .setDescription("Odpowiada Pong!"),
   async execute(interaction) {
-    await interaction.reply(`🏓 Ping wynosi ${Date.now() - interaction.createdTimestamp}ms.`);
+    await interaction.reply({ content: `🏓 Ping wynosi ${Date.now() - interaction.createdTimestamp}ms.`, ephemeral: true });
   },
 };
